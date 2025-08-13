eb8970c6edef9e595080e8edc848041c9720d635
Bruno de Fraga <brunofragaa97@gmail.com>
2025-08-13 18:11:05 -0300

    Inserido 0 no resultado da consulta antes vinha null, ajustado para mostrar negativamente contas que fazem parte de desconto

--- PL SQL/Consulta Incidencia Analitica/PRC prc_gera_tabela_pivot_contas_incid.sql
+++ PL SQL/Consulta Incidencia Analitica/PRC prc_gera_tabela_pivot_contas_incid.sql
@@ -1,9 +1,9 @@
---PRC prc_gera_tabela_pivot_contas_incidencia_dinamica 
 create or replace PROCEDURE prc_gera_tabela_pivot_contas_incidencia_dinamica (
     in_seq_execucao IN NUMBER
 )
 IS
-    aux_contas varchar2(4000);
+    aux_contas clob;
+    aux_contas_select clob;
 BEGIN
     BEGIN
         EXECUTE IMMEDIATE 'drop table tmp_consulta_incidencias_analiticas';
@@ -12,14 +12,23 @@ BEGIN
                 RAISE;
             END IF;
         END;
-        
+
         select listagg (distinct conta || case when conta <> -1 then ' as "Conta: ' || conta || '"' else ' as "Total"' end, ', ') within group (order by conta desc)
-        INTO   aux_contas 
+        INTO   aux_contas
         from table(PKG_RELATORIOS_PAD_DEV.con_incidencias_analitica(in_seq_execucao));
         
+        select listagg(distinct case when conta <> -1 then 'nvl("Conta: ' || conta || '", 0) as "Conta: ' || conta || '"' else 'nvl("Total", 0) as "Total"' end, ', ') within group (order by conta desc)
+        INTO   aux_contas_select
+        from table(PKG_RELATORIOS_PAD_DEV.con_incidencias_analitica(in_seq_execucao));
+
         execute immediate '
         create table tmp_consulta_incidencias_analiticas as
-        select *
+        select periodo,
+        cpf,
+        matricula,
+        nome,
+        tipofolha,
+        ' || aux_contas_select || '
         from (
             select *
             from table (PKG_RELATORIOS_PAD_DEV.con_incidencias_analitica(' || in_seq_execucao || '))
