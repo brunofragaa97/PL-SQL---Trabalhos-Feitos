8e6987fd061b5bfb225b17766144f4025e3590f1
Bruno de Fraga <brunofragaa97@gmail.com>
2025-08-14 14:11:22 -0300

    resolvido problemas com null, onde agora retornam o valor zero | formatado campo de matricula para nao ficar como miliar | formatado campo de valores para serem mostradados casas decimais

--- PL SQL/Consulta Incidencia Analitica/PRC prc_gera_tabela_pivot_contas_incid.sql
+++ PL SQL/Consulta Incidencia Analitica/PRC prc_gera_tabela_pivot_contas_incid.sql
@@ -17,26 +17,45 @@ BEGIN
         INTO   aux_contas
         from table(PKG_RELATORIOS_PAD_DEV.con_incidencias_analitica(in_seq_execucao));
         
-        select listagg(distinct case when conta <> -1 then 'nvl("Conta: ' || conta || '", 0) as "Conta: ' || conta || '"' else 'nvl("Total", 0) as "Total"' end, ', ') within group (order by conta desc)
+        /*select listagg(distinct case when conta <> -1 then 'round(nvl("Conta: ' || conta || '", 0), 2) as "Conta: ' || conta || '"' else 'round(nvl("Total", 0), 2) as "Total"' end, ', ') within group (order by conta desc)
         INTO   aux_contas_select
-        from table(PKG_RELATORIOS_PAD_DEV.con_incidencias_analitica(in_seq_execucao));
+        from table(PKG_RELATORIOS_PAD_DEV.con_incidencias_analitica(in_seq_execucao));*/
+        
+        for conta in (
+            select distinct conta 
+            from table(PKG_RELATORIOS_PAD_DEV.con_incidencias_analitica(in_seq_execucao))
+            order by conta desc
+        ) loop
+            if conta.conta >= 0 then 
+                aux_contas_select := case 
+                    when aux_contas_select is null then 'round(nvl("Conta: ' || conta.conta || '", 0), 2) as "Conta: ' || conta.conta || ':n.2"'
+                    else aux_contas_select || ', round(nvl("Conta: ' || conta.conta || '", 0), 2) as "Conta: ' || conta.conta || ':n.2"'
+                end;
+            else 
+                aux_contas_select := aux_contas_select || ', round(nvl("Total", 0), 2) as "Total:n.2"';
+            end if;
+        end loop;
 
-        execute immediate '
-        create table tmp_consulta_incidencias_analiticas as
-        select periodo,
-        cpf,
-        matricula,
-        nome,
-        tipofolha,
-        ' || aux_contas_select || '
-        from (
-            select *
-            from table (PKG_RELATORIOS_PAD_DEV.con_incidencias_analitica(' || in_seq_execucao || '))
-        ) pivot (
-            sum(NVL(valor, 0))
-            for conta in (
-                ' || aux_contas || '
-                )
-            )
-        ';
+        EXECUTE IMMEDIATE '
+    CREATE TABLE tmp_consulta_incidencias_analiticas 
+    AS
+    SELECT 
+           periodo,
+           cpf,
+           REPLACE(matricula, ''.'', '''') AS matricula,
+           nome,
+           tipofolha,
+           ' || aux_contas_select || '
+    FROM (
+        SELECT *
+        FROM TABLE (
+            PKG_RELATORIOS_PAD_DEV.con_incidencias_analitica(' || in_seq_execucao || ')
+        )
+    ) PIVOT (
+        SUM(VALOR)
+        FOR conta IN (
+            ' || aux_contas || '
+        )
+    )
+';
     END;
